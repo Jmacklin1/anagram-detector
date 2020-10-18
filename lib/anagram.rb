d093b5b9e0e3d78f60fbc8018fa8c7173e10caa7
Jmacklin1 <Macklin.Jason@gmail.com>
2020-10-18 17:25:07 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(string)
+        
+        string.select do |w| 
+            w.split("").sort == word.split("").sort
+        end
+
+
+        #CREATE NEW ARRAy
+#have new array take in words from match that are sorted
+# have the sorted words MATCH with array give using 
+# %w(some words) allows wordsin array to be sorted quickly 
+    end
+end
